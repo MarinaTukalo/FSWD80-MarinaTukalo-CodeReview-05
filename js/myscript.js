ddd988878ac18597dded47bd249dd5c35aaca2b4
Marina Tukalo <marina-tukalo@hotmail.com>
2019-10-17 15:29:55 +0200

    modified version of codereview 5

--- js/myscript.js
+++ js/myscript.js
@@ -1,23 +1,66 @@
 //List of movies
 	var output = '';
-	for(var i = 0; i < dataMovie.length; i++) {
+	for(let i = 0; i < dataMovie.length; i++) {
    		output += "<div class='d-flex col-xs-11 col-sm-11 col-lg-5 border p-2 m-2 bg-black'><img class='pull-left img-responsive' width=100% src='" + dataMovie[i].image + "'>";
    		output += "<p class='d-table m-2 p-2'>" + dataMovie[i].name + "<br><br>";
-    	output += "Description: " + dataMovie[i].about + "</p></div>";
+    	output += "Description: " + dataMovie[i].about + "</p><br>";
+    	output += "<button class='row like_button btn d-flex align-self-end' type='button'>Like <img src='img/like.png' width=30px><span id='result' class='count text-light ml-2 px-2'>"+dataMovie[i].like+"</span></button></div>";
 	}
 	document.getElementById('data_movie').innerHTML = output;
 
-// Like-button
-	$(".'d-table m-2 p-2'").append("<button type='button' id='likes' onclick='button()'>Like</button>");
-	$("#likes").css({"background-color":"green", "padding":"10px","margin":"5px"});
+	var btns = document.getElementsByClassName("like_button");
 
-	var likes = 0;
-	function button() {
-		document.getElementById("likes").innerHTML=likes;
-		likes=likes+1;
-		console.log(likes);
+	for(let i in btns){
+		btns[i].addEventListener("click",function(){
+			Like(dataMovie[i],i) // dataMovie[i] is object and i is for index
+			console.log(dataMovie[i],i);
+		})
 	}
 
+	function Like(obj,i){
+		let like = obj.like; // "like" quantity in json
+		like++; //add one more value to qtt in json
+		document.getElementsByClassName("count")[i].innerHTML= like;
+	}
+
+/* 2 version
+	$(".like_button").on("click", function(){
+		var counter = $(this).after().find(".count").text();
+		console.log(counter);
+		counter++;
+		$(this).after().find(".count").text(counter);
+	});
+*/
+
+// Like-button array 3 version, more suitable for shopping cart
+	/*
+	var likebtn = [];
+
+	function addLikes(obj){
+		if(obj.like == 0){
+			likebtn.push(obj);				
+			obj.like++;
+			showLike();
+		}else{
+			obj.like++;
+			showLike();		
+		}
+	}
+
+	var btns = document.getElementsByClassName("like_button");
+	for(let i = 0; i< btns.length; i++){
+		btns[i].addEventListener("click", function(){
+			addLikes(dataMovie[i])
+		});
+	}
+
+	function showLike(){
+		document.getElementById("result").innerHTML = ``;
+		for(let i=0; i<likebtn.length; i++){
+		document.getElementById("result").innerHTML += `${dataMovie[i].like}`
+		}
+	}
+	*/
 
 // Select function
 
